cb6b86430e99c04bb4ec9df01d4f7e7f7f5297fb
YassineZhang <57878778@qq.com>
2024-02-12 16:44:20 +0800

    feat: update timeline page.

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -1,1 +1,198 @@
-<template>Timeline</template>
+<template>
+  <section class="relative min-h-screen">
+    <header class="bg-white">
+      <nav
+        class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div class="flex items-center gap-x-12">
+          <NuxtLink to="/" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img class="h-8 w-auto" src="/favicon.svg" alt="" />
+          </NuxtLink>
+          <div class="hidden lg:flex lg:gap-x-12">
+            <NuxtLink
+              v-for="item in navigation"
+              :key="item.name"
+              :to="item.href"
+              class="text-sm font-semibold leading-6 text-gray-900"
+              >{{ item.name }}</NuxtLink
+            >
+          </div>
+        </div>
+        <div class="flex lg:hidden">
+          <button
+            type="button"
+            class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = true"
+          >
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="hidden lg:flex">
+          <NuxtLink to="/" class="text-sm font-semibold leading-6 text-gray-900"
+            >回到主页 <span aria-hidden="true">&rarr;</span></NuxtLink
+          >
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        class="lg:hidden"
+        @close="mobileMenuOpen = false"
+        :open="mobileMenuOpen"
+      >
+        <div class="fixed inset-0 z-10" />
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        >
+          <div class="flex items-center justify-between">
+            <NuxtLink to="/" class="-m-1.5 p-1.5">
+              <span class="sr-only">Your Company</span>
+              <img class="h-8 w-auto" src="/favicon.svg" alt="" />
+            </NuxtLink>
+            <button
+              type="button"
+              class="-m-2.5 rounded-md p-2.5 text-gray-700"
+              @click="mobileMenuOpen = false"
+            >
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                <a
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :href="item.href"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >{{ item.name }}</a
+                >
+              </div>
+              <div class="py-6">
+                <NuxtLink
+                  to="/"
+                  class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >回到主页</NuxtLink
+                >
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+    <div
+      class="fixed inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+      aria-hidden="true"
+    />
+    <div class="m-auto max-w-lg flow-root py-12 px-5">
+      <ul role="list" class="-mb-8">
+        <li v-for="(event, eventIdx) in timeline" :key="event.id">
+          <div class="relative pb-8">
+            <span
+              v-if="eventIdx !== timeline.length - 1"
+              class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200"
+              aria-hidden="true"
+            />
+            <div class="relative flex space-x-3">
+              <div>
+                <span
+                  :class="[
+                    event.iconBackground,
+                    'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white',
+                  ]"
+                >
+                  <component
+                    :is="event.icon"
+                    class="h-5 w-5 text-white"
+                    aria-hidden="true"
+                  />
+                </span>
+              </div>
+              <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                <div>
+                  <p class="text-sm text-gray-500">
+                    {{ event.content }}
+                    <a :href="event.href" class="font-medium text-gray-900">{{
+                      event.target
+                    }}</a>
+                  </p>
+                </div>
+                <div class="whitespace-nowrap text-right text-sm text-gray-500">
+                  <time :datetime="event.datetime">{{ event.date }}</time>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { CheckIcon, HandThumbUpIcon, UserIcon } from "@heroicons/vue/20/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  // { name: '主页', href: '/' },
+  { name: "演示中心", href: "/dashboard" },
+];
+
+const mobileMenuOpen = ref(false);
+
+const timeline = [
+  {
+    id: 1,
+    content: "Applied to",
+    target: "Front End Developer",
+    href: "#",
+    date: "Sep 20",
+    datetime: "2020-09-20",
+    icon: UserIcon,
+    iconBackground: "bg-gray-400",
+  },
+  {
+    id: 2,
+    content: "Advanced to phone screening by",
+    target: "Bethany Blake",
+    href: "#",
+    date: "Sep 22",
+    datetime: "2020-09-22",
+    icon: HandThumbUpIcon,
+    iconBackground: "bg-blue-500",
+  },
+  {
+    id: 3,
+    content: "Completed phone screening with",
+    target: "Martha Gardner",
+    href: "#",
+    date: "Sep 28",
+    datetime: "2020-09-28",
+    icon: CheckIcon,
+    iconBackground: "bg-green-500",
+  },
+  {
+    id: 4,
+    content: "Advanced to interview by",
+    target: "Bethany Blake",
+    href: "#",
+    date: "Sep 30",
+    datetime: "2020-09-30",
+    icon: HandThumbUpIcon,
+    iconBackground: "bg-blue-500",
+  },
+  {
+    id: 5,
+    content: "Completed interview with",
+    target: "Katherine Snyder",
+    href: "#",
+    date: "Oct 4",
+    datetime: "2020-10-04",
+    icon: CheckIcon,
+    iconBackground: "bg-green-500",
+  },
+];
+</script>
